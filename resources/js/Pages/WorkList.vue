5f4bce9d73cc609daad4cc38bd432da70ab8420e
XeniaTkacheva <xenia1898@gmail.com>
2022-05-12 23:04:34 +0200

    add works page dynamic

--- resources/js/Pages/WorkList.vue
+++ resources/js/Pages/WorkList.vue
@@ -7,13 +7,13 @@ import {Head, Link} from '@inertiajs/inertia-vue3';
 defineProps({
     works: Object,
 });
+
 </script>
 
 <template>
     <Head title="Works"/>
 
     <XenBaseLayout>
-        <!--        <pre>{{ // works }} </pre>-->
         <template #page_background>
             <div class="-z-50 h-full w-screen bg-gray-100 bg-right-top bg-no-repeat fixed"></div>
         </template>
@@ -33,7 +33,6 @@ defineProps({
         </template>
         <ul>
             <li v-for="work in works" class="mb-4">
-                <!--                <pre>{{ work.link }} </pre>-->
                 <div class="sm:pb-12 sm:pt-0 grid-work min-h-[60vh]">
                     <div
                         class="grid-header rounded-lg  bg-greenGray-light/90 text-2xl font-bold text-white p-2 md:p-5 z-10 mb-2">
@@ -65,13 +64,11 @@ defineProps({
                         </li>
                     </ul>
                     <div class="z-10 grid-desc  min-h-[50px] p-6 text-white bg-greenGray-light/90 rounded-lg mb-2">
-                        Очень красивый сайт на Вордпрессе
+                        {{ work.description }}
                     </div>
-                    <div
-                        class="grid-image min-h-[30vh] sm:min-h-[50vh] p-4 bg-clip-padding rounded-lg work-img bg-white/90 box-border border md:border-4 border-greenGray-light/40 w-full h-auto bg-blend-luminosity hover:bg-blend-normal bg-[url('@@/work3.jpg')] mb-2">
-                        <!--                <img src="@@/work5.jpg" alt="photo5" class=" w-full h-auto block rounded-lg bg-blue-100/50 bg-blend-luminosity">-->
+                    <div :style="'background-image: url(\'/images/' +  work.work_image +'\')'"
+                        class="grid-image min-h-[30vh] sm:min-h-[50vh] p-4 bg-clip-padding rounded-lg work-img bg-white/90 box-border border md:border-4 border-greenGray-light/40 w-full h-auto bg-blend-luminosity hover:bg-blend-normal mb-2">
                     </div>
-
                 </div>
             </li>
         </ul>
@@ -84,49 +81,15 @@ defineProps({
 </template>
 
 <style scoped>
-.bg-works {
-    background-color: #757575;
-    opacity: 0.1;
-}
 
 .work-img {
-    /*background-color: rgba(47,76,115, 0.7);*/
-    /*background-image: url('@@/work5.jpg');*/
     background-size: cover;
     background-repeat: no-repeat;
 }
 
-/*.work-img {*/
-/*    background-color: yellowgreen;*/
-/*    background-blend-mode: color-dodge;*/
-/*}*/
-/*.work-img-box::before {*/
-/*    content: '';*/
-/*    position: absolute;*/
-/*    top: 0;*/
-/*    left: 0;*/
-/*    right: 0;*/
-/*    bottom: 0;*/
-/*    z-index: 10;*/
-/*    !*background: red; !*rgb(0, 0, 0, 0.9);*!*!*/
-/*    background:  rgba(47,76,115, 0.2) linear-gradient(0deg, rgba(47,76,115, 0.3) 0%, rgb(255, 255, 250, 0) 30%);*/
-/*    border-radius: 0.5rem;*/
-/*}*/
-/*.work-img-box:hover  .work-img-box::before {*/
-/*    opacity: 0;*/
-/*}*/
-
 .grid-work {
     display: block;
 }
-
-/*@media (min-width: 640px) {*/
-/*    .grid-work {*/
-/*        display: grid;*/
-/*        grid-template-columns: 1fr 2fr 1fr 2fr 3fr;*/
-/*        grid-template-rows: auto 1rem 2rem 4fr auto auto 2fr;*/
-/*    }*/
-/*}*/
 
 @media (min-width: 768px) {
     .grid-work {
@@ -137,38 +100,27 @@ defineProps({
 }
 
 .grid-header {
-    /*grid-area: header;*/
     grid-column: 1 / 4;
     grid-row: 1 / 3;
-    /*opacity: 0.5;*/
-    /*align-self: start;*/
 }
 
 .grid-image {
-    /*grid-area: image;*/
     grid-column: 3 / 6;
     grid-row: 2 / 7;
-    /*opacity: 0.5;*/
 }
 
 .grid-desc {
-    /*grid-area: desc;*/
     grid-column: 2 / 4;
     grid-row: 5 / 6;
-    /*opacity: 0.5;*/
 }
 
 .grid-back {
-    /*grid-area: back;*/
     grid-column: 1 / 5;
     grid-row: 4 / 8;
-    /*background: #01325a;*/
-    /*background: #00587E;*/
-    /*opacity: 0.5;*/
 }
 
-.bg-w1 {
-    background-image: url('@@/work1.jpg');
-    background-size: cover;
-}
+/*.bg-w1 {*/
+/*    background-image: url('@@/work1.jpg');*/
+/*    background-size: cover;*/
+/*}*/
 </style>
